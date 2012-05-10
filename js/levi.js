e56277c9347c9f8d5a3c3c41b8ec0e7c735f5bbf
Hans Then <h.then@pythea.nl>
2012-05-10 02:35:22 +0200

    Added levi.js and style.css

---
+++ js/levi.js
@@ -0,0 +1,41 @@
+function initialize() {
+    $('#content form').submit( function(event, ui) { 
+        event.preventDefault();
+        var url = event.currentTarget.action;
+        var url = event.currentTarget.attributes.action.nodeValue;
+        data = $(this).serialize();
+        $.address.value(url + '?' + data); 
+    });
+}
+$(document).ready(function(){
+    initialize();
+    $('body a').live('click', function( event, ui ) {
+        event.preventDefault();
+        var url = event.currentTarget.pathname;
+        var search = event.currentTarget.search;
+        $.address.value(url + search);
+    });
+    $.address.change(function(event){
+        if(event.path == '/') return;
+        document.body.style.cursor = "wait";
+        if($.isEmptyObject(event.parameters)) {
+            type = "GET";
+        } else {
+            type = "POST";
+        }
+        $.ajax({
+            type: type,
+//            url: event.path,
+            url: event.value,
+            data: event.parameters,
+            success: function(html) {
+                $('#content').empty().append(html);
+                initialize();
+                document.body.style.cursor = "default";
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                document.body.style.cursor = "default";
+            }
+        });
+    });
+});
