85fe422cacc1e25752e1d5179acf5848213fdb49
Hans Then <h.then@pythea.nl>
2012-05-30 02:27:40 +0200

    Categories toegevoegd. Initiele scherm bevat nu een lijstje met categorieen. Tonen van foutmeldingen toegevoegd aan js/levi-2.js. Scherm toegevoegd om categorien te koppelen aan crediteuren.

--- js/levi.js
+++ js/levi.js
@@ -1,11 +1,18 @@
+var template = /<!--Template:(.*)-->/
+
+function loadIn(tag, html) {
+    $(tag).empty().append(html);
+    template_name = "";
+    match = html.match(template);
+    if( match ) { template_name = match[1] };
+    initialize();
+}
+
 function initialize() {
     $('#content form').submit( function(event, ui) { 
         event.preventDefault();
         /*var url = event.currentTarget.action;*/
         var url = event.currentTarget.attributes.action.nodeValue;
-/*
-        $.address.value(url + '?' + data); 
-*/
         data = $(this).serialize();
         document.body.style.cursor = "wait";
         $.ajax({
@@ -13,16 +20,17 @@ function initialize() {
             url: url,
             data: data,
             success: function(html, jqXHR) {
-                $('#content').empty().append(html);
-                initialize();
+                loadIn('#content', html);
                 document.body.style.cursor = "default";
             },
             error: function(jqXHR, textStatus, errorThrown) {
+                $('#content').empty().append(textStatus + ' ' + jqXHR.responseText);
                 document.body.style.cursor = "default";
             }
         });
     });
 }
+
 $(document).ready(function(){
     initialize();
     $('body a').live('click', function( event, ui ) {
@@ -41,16 +49,14 @@ $(document).ready(function(){
         }
         $.ajax({
             type: type,
-//            url: event.path,
             url: event.value,
             data: event.parameters,
             success: function(html, textStatus, jqXHR) {
-                $('#content').empty().append(html);
-                initialize();
+                loadIn('#content', html);
                 document.body.style.cursor = "default";
             },
             error: function(jqXHR, textStatus, errorThrown) {
-                $('#content').empty().append(html);
+                $('#content').empty().append(textStatus + ' ' + jqXHR.responseText);
                 initialize();
                 document.body.style.cursor = "default";
             }
